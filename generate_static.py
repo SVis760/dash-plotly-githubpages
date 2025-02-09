dbbc7c1b2d4ed2534517cf8d9ea18c784174c741
SVis760 <sablina.vis@gmail.com>
2025-02-09 12:16:46 +0100

    nn

--- generate_static.py
+++ generate_static.py
@@ -1,23 +1,27 @@
 import os
 import time
 import threading
+import signal
+import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from flask import request
 from app import app  # Import your Dash app
 
-# Function to stop the Flask server after rendering
-def shutdown_server():
-    """Send a shutdown request to the Flask server."""
-    func = request.environ.get("werkzeug.server.shutdown")
-    if func:
-        func()
+# Global variable to store the Flask thread
+server_thread = None
 
 def run_dash():
     """Run the Dash app in a separate thread."""
     app.run_server(debug=False, port=8050, use_reloader=False)
+
+def stop_dash():
+    """Stop the Dash app by sending a SIGINT (CTRL+C)."""
+    global server_thread
+    if server_thread:
+        print("Stopping Dash app...")
+        os.kill(os.getpid(), signal.SIGINT)  # Simulate CTRL+C to stop Flask
 
 def save_static_html():
     """Launch Dash app, visit it with Selenium, and save the page as HTML."""
@@ -33,8 +37,9 @@ def save_static_html():
 
     try:
         # Start the Dash app in a separate thread
+        global server_thread
         server_thread = threading.Thread(target=run_dash)
-        server_thread.daemon = True  # Allows the script to exit when done
+        server_thread.daemon = True  # Allows script to exit
         server_thread.start()
 
         # Wait for the server to start
@@ -48,11 +53,9 @@ def save_static_html():
         with open("static/index.html", "w", encoding="utf-8") as f:
             f.write(driver.page_source)
 
-        # Send shutdown request
-        shutdown_server()
-
     finally:
         driver.quit()
+        stop_dash()  # Stop the Dash app after saving
 
 if __name__ == "__main__":
     save_static_html()
